3243c166c5d6dc6ca30421247237225b1108e900
mnvsngv <mnvsngv@gmail.com>
2017-04-29 15:33:34 +0530

    Initial working version

--- src/pages/ballots/ballots.ts
+++ src/pages/ballots/ballots.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AlertController,NavController } from 'ionic-angular';
+//import * as firebase from "firebase";
 
 @Component({
   selector: 'page-ballots',
@@ -7,39 +8,86 @@ import {AlertController,NavController } from 'ionic-angular';
 })
 
 export class Ballots {
-submitPrep:Boolean=false;
-submitTT:Boolean=false;
-submitEval:Boolean=false;
-pspeaker: String;
-pspeakers: Array<{name: string}>;
-ttspeaker: String;
-ttspeakers: Array<{name: string}>;
-espeaker: String;
-espeakers: Array<{name: string}>;
+  submitPrep:Boolean=false;
+  submitTT:Boolean=false;
+  submitEval:Boolean=false;
+  submitBig3:Boolean=false;
+  pspeaker: String;
+  pindex: number;
+  pspeakers: Array<{name: string, index: number}>;
+  ttspeaker: String;
+  ttindex: number;
+  ttspeakers: Array<{name: string, index: number}>;
+  espeaker: String;
+  eindex: number;
+  espeakers: Array<{name: string, index: number}>;
+  bspeaker: String;
+  bindex: number;
+  bspeakers: Array<{role: string, name: string, index: number}>;
+
 // result:Boolean=false;
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     let context = this;
+    let firebaseRef = firebase.database().ref('/roleBearers');
 
-    firebase.database().ref('/roleBearers').once('value').then(function(snapshot) {
+    firebaseRef.once('value').then(function(snapshot) {
       let roleBearers = snapshot.val();
-      let preparedSpeakers = roleBearers.preparedSpeakers;
 
+      let preparedSpeakers = roleBearers.preparedSpeakers;
       context.pspeakers = [];
-      for(let speaker of preparedSpeakers) {
-        context.pspeakers.push({name: speaker.name});
+
+      let index: number = context.getFromLocalStorage("preparedSpeaker");
+      if(index != null) {
+        context.submitPrep=true;
       }
 
-      context.ttspeakers = [
-        { name: 'TT1' },
-        { name: 'TT2'},
-        { name: 'TT3'},
-        { name: 'TT4' }
-      ];
+      let i = 0;
+      for(let speaker of preparedSpeakers) {
+        if(i === index) context.pspeaker = speaker.name;
+        context.pspeakers.push( {name: speaker.name, index: i++} );
+      }
 
-      context.espeakers = [
-        { name: 'E1' },
-        { name: 'E2' }
-      ];
+      let ttSpeakers = roleBearers.ttSpeakers;
+      context.ttspeakers = [];
+
+      index = context.getFromLocalStorage("ttSpeaker");
+      if(index != null) {
+        context.submitTT=true;
+      }
+
+      i = 0;
+      for(let speaker of ttSpeakers) {
+        if(i === index) context.ttspeaker = speaker.name;
+        context.ttspeakers.push( {name: speaker.name, index: i++} );
+      }
+
+      let evaluators = roleBearers.evaluators;
+      context.espeakers = [];
+
+      index = context.getFromLocalStorage("evaluator");
+      if(index != null) {
+        context.submitEval=true;
+      }
+
+      i = 0;
+      for(let speaker of evaluators) {
+        if(i === index) context.espeaker = speaker.name;
+        context.espeakers.push( {name: speaker.name, index: i++} );
+      }
+
+      let big3 = roleBearers.big3;
+      context.bspeakers = [];
+
+      index = context.getFromLocalStorage("big3");
+      if(index != null) {
+        context.submitBig3=true;
+      }
+
+      i = 0;
+      for(let speaker of big3) {
+        if(i === index) context.bspeaker = speaker.name;
+        context.bspeakers.push( {role: speaker.role, name: speaker.name, index: i++} );
+      }
     });
 
 
@@ -52,9 +100,9 @@ espeakers: Array<{name: string}>;
       buttons: [{
         text:'OK',
         handler : data=>{
-console.log("Hi");
-      }
-    }]
+          console.log("Hi");
+        }
+      }]
     });
     alert.present();
   }
@@ -68,90 +116,154 @@ console.log("Hi");
         handler : data=>{
           switch(sp){
             case 'prep':{
-      this.submitPrep=true;
-      break;
+              this.submitPrep=true;
+              firebase.database().ref('/roleBearers/preparedSpeakers/'+this.pindex+'/votes').transaction(function(snapshot) {
+                return snapshot + 1;
+              });
+              this.addToLocalStorage("preparedSpeaker", this.pindex);
+              break;
             }
             case 'tt':{
-      this.submitTT=true;
-            break;
-          }
+              this.submitTT=true;
+              firebase.database().ref('/roleBearers/ttSpeakers/'+this.ttindex+'/votes').transaction(function(snapshot) {
+                return snapshot + 1;
+              });
+              this.addToLocalStorage("ttSpeaker", this.ttindex);
+              break;
+            }
             case 'eval':{
-      this.submitEval=true;
-          break;
-          }
+              this.submitEval=true;
+              firebase.database().ref('/roleBearers/evaluators/'+this.eindex+'/votes').transaction(function(snapshot) {
+                return snapshot + 1;
+              });
+              this.addToLocalStorage("evaluator", this.eindex);
+              break;
+            }
+            case 'big3':{
+              this.submitBig3=true;
+              firebase.database().ref('/roleBearers/big3/'+this.bindex+'/votes').transaction(function(snapshot) {
+                return snapshot + 1;
+              });
+              this.addToLocalStorage("big3", this.bindex);
+              break;
+            }
           }
 
-      // return true;
-      }
-    },{
+          // return true;
+        }
+      },{
         text:'Cancel',
         handler : data=>{
-console.log("Cancel");
+          console.log("Cancel");
 // this.submit=false;
 // return false;
-      }
-    }]
+        }
+      }]
     });
     alert.present();
     // return promise(result);
   }
 
-submitP(sp){
-  switch(sp){
-            case 'prep':{
-       if(this.pspeaker!=null){
-  this.showAlertSubmit(sp,this.pspeaker);
-  // console.log('if'+ this.submit);
-}
-else{
-this.showAlertSelect();
-}
- break;
-            }
-            case 'tt':{ if(this.ttspeaker!=null){
-  this.showAlertSubmit(sp,this.ttspeaker);
+  submitP(sp){
+    switch(sp){
+      case 'prep':{
+        if(this.pspeaker!=null){
+          this.showAlertSubmit(sp,this.pspeaker);
+          // console.log('if'+ this.submit);
+        }
+        else{
+          this.showAlertSelect();
+        }
+        break;
+      }
+      case 'tt':{ if(this.ttspeaker!=null){
+        this.showAlertSubmit(sp,this.ttspeaker);
 // console.log("SubmitP");
-  // console.log('if'+ this.submit);
-}
-else{
-this.showAlertSelect();
-}  break;
-            }
-            case 'eval':{
-               if(this.espeaker!=null){
-  this.showAlertSubmit(sp,this.espeaker);
-  // console.log('if'+ this.submit);
-}
-else{
-this.showAlertSelect();
-}
-            break;
-           }
+        // console.log('if'+ this.submit);
+      }
+      else{
+        this.showAlertSelect();
+      }  break;
+      }
+      case 'eval':{
+        if(this.espeaker!=null){
+          this.showAlertSubmit(sp,this.espeaker);
+          // console.log('if'+ this.submit);
+        }
+        else{
+          this.showAlertSelect();
+        }
+        break;
+      }
+
+      case 'big3':{
+        if(this.bspeaker!=null){
+          this.showAlertSubmit(sp,this.bspeaker);
+          // console.log('if'+ this.submit);
+        }
+        else{
+          this.showAlertSelect();
+        }
+        break;
+      }
+    }
+
+  }
+  getP(sp){
+    console.log("getP"+sp);
+// return true;
+    switch(sp){
+      case 'prep':{
+        return this.submitPrep;
+      }
+      case 'tt':{
+        return this.submitTT;
+      }
+      case 'eval':{
+        return this.submitEval;
+      }
+      case 'big3':{
+        return this.submitBig3;
+      }
+    }
+  }
+  voteP(p){
+    this.pspeaker=p.name;
+    this.pindex = p.index;
+  }
+  voteTT(p){
+    this.ttspeaker=p.name;
+    this.ttindex = p.index;
+  }
+  voteE(p){
+    this.espeaker=p.name;
+    this.eindex = p.index;
+  }
+  voteB(p){
+    this.bspeaker=p.name;
+    this.bindex = p.index;
+  }
+
+
+  addToLocalStorage(name, value) {
+  let object = {value: value, timestamp: new Date().getTime()};
+  localStorage.setItem(name, JSON.stringify(object));
 }
 
+  getFromLocalStorage(name) {
+  let object = localStorage.getItem(name);
+  if(object == null) {
+    return null;
+  } else {
+    let now = new Date().getTime();
+    let cookie = JSON.parse(object);
+    let timestamp = cookie.timestamp;
+    if(now - timestamp > 60*60*1000 /*1 hour */) {
+      localStorage.removeItem(name);
+      return null;
+    } else {
+      return cookie.value;
+    }
+  }
 }
-getP(sp){
-console.log("getP"+sp);
-// return true;
-switch(sp){
-            case 'prep':{
-      return this.submitPrep;
-            }
-            case 'tt':{
-      return this.submitTT;
-            }
-            case 'eval':{
-    return this.submitEval;
-            }
-}
-}
- voteP(p){
-   this.pspeaker=p.name;
- }
- voteTT(p){
-   this.ttspeaker=p.name;
- }
- voteE(p){
-   this.espeaker=p.name;
- }
 }
